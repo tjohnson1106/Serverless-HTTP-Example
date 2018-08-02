77da2aed08092cc792eb0312cceef21df19add54
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-02 15:02:38 -0400

    created image resize and hello world function

---
+++ handler.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "V. 0.1",
+      input: event
+    })
+  };
+
+  callback( null, response );
+  
+
+module.exports.imageResize = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "resized your image",
+      input: event
+    })
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
